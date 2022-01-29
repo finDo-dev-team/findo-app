67fab4c9bb1c351e0a68392300698ad7128c80c2
Gabriel Jestin <jestingabriel@gmail.com>
2022-01-29 15:46:20 +0100

    ajout possibilité null

--- app/Tasks/ODPExtractor.php
+++ app/Tasks/ODPExtractor.php
@@ -10,26 +10,24 @@ class ODPExtractor
 {
     private static $queryNext2WeeksEvents = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E+%23now()+AND+date_start+%3C%3D+%23now(weeks%3D%2B2)&rows=1000';
 
-    private array $records;
-
     public function __invoke()
     {
-        Log::info('ODPExtractor started runing...');
+        Log::info('ODPExtractor started running...');
 
-        $this->records = array();
+        $records = array();
 
         $response = Http::get(ODPExtractor::$queryNext2WeeksEvents);
 
-        $this->records = $response['records'];
+        $records = $response['records'];
 
-        foreach ($this->records as $record) {
+        foreach ($records as $record) {
             $this->createOrUpdateEventFromRecord($record);
         }
 
-        Log::info('ODPExtractor terminated!');
+        Log::info('ODPExtractor successfully executed!');
     }
 
-    private function createOrUpdateEventFromRecord(array $record): void
+    private static function createOrUpdateEventFromRecord(array $record): void
     {
         $fields = $record['fields'];
 
@@ -37,41 +35,42 @@ class ODPExtractor
 
         if($odpEvent == NULL) $odpEvent = new ODPEvent();
 
-        $odpEvent->odpID = $fields['id'];
-        $odpEvent->url = $fields['url'];
-        $odpEvent->title = $fields['title'];
-        $odpEvent->lead_text = $fields['lead_text'];
-        $odpEvent->description = $fields['description'];
-        $odpEvent->date_start = $fields['date_start'];
-        $odpEvent->date_end = $fields['date_end'];
-        $odpEvent->occurrences = $fields['occurrences'];
-        $odpEvent->date_description = $fields['date_description'];
-        $odpEvent->cover_url = $fields['cover_url'];
-        $odpEvent->cover_alt = $fields['cover_alt'];
-        $odpEvent->cover_credit = $fields['cover_credit'];
-        $odpEvent->tags = $fields['tags'];
-        $odpEvent->address_name = $fields['address_name'];
-        $odpEvent->address_street = $fields['address_street'];
-        $odpEvent->address_zipcode = $fields['address_zipcode'];
-        $odpEvent->address_city = $fields['address_city'];
-        $odpEvent->latitude = $fields['lat_lon'][0];
-        $odpEvent->longitude = $fields['lat_lon'][1];
-        $odpEvent->pmr = $fields['pmr'];
-        $odpEvent->blind = $fields['blind'];
-        $odpEvent->deaf = $fields['deaf'];
-        $odpEvent->transport = $fields['transport'];
-        $odpEvent->contact_url = $fields['contact_url'];
-        $odpEvent->contact_phone = $fields['contact_phone'];
-        $odpEvent->contact_mail = $fields['contact_mail'];
-        $odpEvent->contact_facebook = $fields['contact_facebook'];
-        $odpEvent->contact_twitter = $fields['contact_twitter'];
-        $odpEvent->price_type = $fields['price_type'];
-        $odpEvent->price_detail = $fields['price_detail'];
-        $odpEvent->access_type = $fields['access_type'];
-        $odpEvent->odp_updated_at = $fields['updated_at'];
-        $odpEvent->programs = $fields['programs'];
-        $odpEvent->address_url = $fields['address_url'];
-        $odpEvent->title_event = $fields['title_event'];
+        $odpEvent->odpID = $fields['id'] ?? null;
+        $odpEvent->url = $fields['url'] ?? null;
+        $odpEvent->title = $fields['title'] ?? null;
+        $odpEvent->lead_text = $fields['lead_text'] ?? null;
+        $odpEvent->description = $fields['description'] ?? null;
+        $odpEvent->date_start = $fields['date_start'] ?? null;
+        $odpEvent->date_end = $fields['date_end'] ?? null;
+        $odpEvent->occurrences = $fields['occurrences'] ?? null;
+        $odpEvent->date_description = $fields['date_description'] ?? null;
+        $odpEvent->cover_url = $fields['cover_url'] ?? null;
+        $odpEvent->cover_alt = $fields['cover_alt'] ?? null;
+        $odpEvent->cover_credit = $fields['cover_credit'] ?? null;
+        $odpEvent->tags = $fields['tags'] ?? null;
+        $odpEvent->address_name = $fields['address_name'] ?? null;
+        $odpEvent->address_street = $fields['address_street'] ?? null;
+        $odpEvent->address_zipcode = $fields['address_zipcode'] ?? null;
+        $odpEvent->address_city = $fields['address_city'] ?? null;
+        $odpEvent->latitude = $fields['lat_lon'][0] ?? null;
+        $odpEvent->longitude = $fields['lat_lon'][1] ?? null;
+        $odpEvent->pmr = $fields['pmr'] ?? null;
+        $odpEvent->blind = $fields['blind'] ?? null;
+        $odpEvent->deaf = $fields['deaf'] ?? null;
+        $odpEvent->transport = $fields['transport'] ?? null;
+        $odpEvent->contact_url = $fields['contact_url'] ?? null;
+        $odpEvent->contact_phone = $fields['contact_phone'] ?? null;
+        $odpEvent->contact_mail = $fields['contact_mail'] ?? null;
+        $odpEvent->contact_facebook = $fields['contact_facebook'] ?? null;
+        $odpEvent->contact_twitter = $fields['contact_twitter'] ?? null;
+        $odpEvent->price_type = $fields['price_type'] ?? null;
+        $odpEvent->price_detail = $fields['price_detail'] ?? null;
+        $odpEvent->access_type = $fields['access_type'] ?? null;
+        $odpEvent->odp_updated_at = $fields['updated_at'] ?? null;
+        $odpEvent->programs = $fields['programs'] ?? null;
+        $odpEvent->address_url = $fields['address_url'] ?? null;
+        $odpEvent->title_event = $fields['title_event'] ?? null;
+
         $odpEvent->save();
     }
 
