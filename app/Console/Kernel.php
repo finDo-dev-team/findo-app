67a9bb4d82c8e3ee51496589b576eede2022f884
Gabriel Jestin <jestingabriel@gmail.com>
2022-01-29 15:45:56 +0100

    ajout nom au schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(new ODPExtractor)
                  ->timezone('Europe/Paris')
+                 ->name('ODP Extract')
                  ->everyMinute();
     }
 
