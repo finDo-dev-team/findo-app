8612960ad6e951c52d7e8661cb95bdd57f401617
samysi <samysi2216@gmail.com>
2022-02-08 10:11:10 +0100

    ajout controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+
+    public function diagram () {
+
+		// Les catégories avec les produits. On peut aussi utiliser la méthode withCount() d'Eloquent
+		$ODPEvents = ODPEvent::with("events")->get();
+
+		// La vue "diagram" avec les catégories
+		return view("diagram", compact('ODPEvents'));
+    }
 }
