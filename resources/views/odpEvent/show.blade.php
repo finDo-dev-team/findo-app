3f012d1c14652e8207a78780bfef7e1d741d8592
Gabriel Jestin <jestingabriel@gmail.com>
2022-02-08 10:05:53 +0100

    changement minuers CSS

--- resources/views/odpEvent/show.blade.php
+++ resources/views/odpEvent/show.blade.php
@@ -5,16 +5,17 @@
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="mx-auto sm:px-6 lg:px-8 mt-4">
+        <div class="mx-auto sm:px-6 lg:px-8">
 
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="shadow-md rounded-lg overflow-hidden">
+                <div class="shadow-md overflow-hidden">
                     <div class="mx-auto max-h-full w-6" style="width: 30rem">
                         <img src="{{ $odpEvent->cover_url }}"
-                            alt="{{ $odpEvent->cover_alt }}">
+                            alt="{{ $odpEvent->cover_alt }}"
+                            class="rounded-full">
                     </div>
 
-                    <table class="table-auto min-w-full divide-y divide-black">
+                    <table class="table-auto min-w-full divide-y divide-black mt-2">
                         <thead class="bg-white">
                             <tr>
                                 <th colspan="2" class="px-6 py-3 text-left text-xl font-medium uppercase tracking-wider">
