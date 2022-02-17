36b5480785c70d0f8ad9dae62e403f07cda74a4d
AnissElarj <aniss@LAPTOP-T7DL6UBM.localdomain>
2022-02-17 13:03:38 +0100

    Ajout de la page recommandation

---
+++ app/Http/Controllers/RecommController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ODPEvent;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+use App\Providers\RouteServiceProvider;
+
+class RecommController extends Controller
+{
+
+    public function index() {
+
+		$odpEvents = ODPEvent::all();
+        $eventCount = ODPEvent::count();
+
+         // Comptage et listing des tags
+
+        $listeTags = ["Clubbing","Atelier","Humour","Concert","Musique","Danse","Théâtre","Conférence","Enfants","Sport","Cinéma","Loisirs","Littérature","Spectacle musical","Balade","Histoire","Nature","Art contemporain","Expo","LGBT","Peinture","Photo","Innovation","Cirque","BD","Sciences","Solidarité","Salon","Street-art","Gourmand"];
+        $countTags = array();
+
+        //Recup ce qui est liker par les autres
+
+
+
+        foreach ($listeTags  as $tag) {
+            $countTags[$tag]= ODPEvent::where('tags', 'like','%'.$tag.'%')->count();
+        }
+
+
+        
+        // Comptage des likes
+
+        $currentUser = Auth::user();
+        $likeCount = array();
+        foreach ($listeTags  as $tag1) {
+            $likeCount[$tag1]= $currentUser->likedEvents()->where('tags', 'like','%'.$tag1.'%')->count();
+        }
+         
+        return View::make('dashboard', [
+            'odpEvents' => $odpEvents,
+            'eventCount' => $eventCount,
+            'listeTags' => $listeTags,
+            'countTags' => $countTags,
+            'likeCount' => $likeCount
+        ]);
+    }
+}
